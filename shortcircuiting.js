6d2c54218664b15513671d22f345da8a923fa201
aneeshpatne <aneeshpatne@gmail.com>
2024-06-29 18:19:38 +0530

    shortcircuit

--- shortcircuiting.js
+++ shortcircuiting.js
@@ -1,4 +1,18 @@
 console.log(true && 'hello');
 console.log(false && 'hello');
-const isPresent = false ;
+const isPresent = true ; //same as false
 console.log(isPresent && 'Student Name')
+console.log(isPresent || 'Student Name')
+const a = 0; 
+console.log(a || 'Sorry doenst exist');
+const unwanted = 1 ;
+console.log(unwanted && 'error Exists')
+const exists = null;
+console.log(exists ?? 'doesnt Exists')
+
+const hello = (firstName) => {
+    let name = firstName ?? 'Guest' ; 
+    console.log(`Hello ${name}`) ; 
+};
+hello();
+hello('Aneesh');
